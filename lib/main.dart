d1062bff182f485f4a48163d10ee40fc06c6a554
emson <wulfsberg.emma@gmail.com>
2025-10-17 14:03:14 +0200

    firebse_fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+   );
   await initializeDateFormatting('sv_SE', null); // Initiera svenska locale
   runApp(const DialoglasningsApp());
 }
