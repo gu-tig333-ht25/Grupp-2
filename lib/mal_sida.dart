d176f9089070cc3521e5d3fc99dedc181739c78d
gusasenljo <gusasenljo@student.gu.se>
2025-10-28 11:31:35 +0100

    ska fungera nu pls v2

--- lib/mal_sida.dart
+++ lib/mal_sida.dart
@@ -13,6 +13,14 @@ class MalSida extends StatefulWidget {
 class _MalSidaState extends State<MalSida> {
   Filtrering _valdFiltrering = Filtrering.alla;
 
+@override
+void initState() {
+  super.initState();
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    Provider.of<MalProvider>(context, listen: false).loadGoalsFromFirestore();
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
